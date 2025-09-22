e6f6384afbd026267173fa84b39d91f6ad565b53
jbrownfield-0322 <jcbrownfield@outlook.com>
2025-09-22 17:34:27 -0500

    Update scheduling for live-score-updater and enhance GameScoreService logic
    - Changed the schedule for the live-score-updater to run every hour on Sundays and Mondays, improving score update frequency.
    - Added a new method in GameRepository to find unscored games within a time range.
    - Enhanced the GameScoreService to check for unscored games and adjust the fetching window based on the earliest and latest game times, ensuring scores are fetched during the appropriate time frame.

--- src/main/java/com/nflpickem/pickem/repository/GameRepository.java
+++ src/main/java/com/nflpickem/pickem/repository/GameRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public interface GameRepository extends JpaRepository<Game, Long> {
     List<Game> findByWeek(Integer week);
     List<Game> findByScoredFalseAndKickoffTimeBefore(Instant dateTime);
+    List<Game> findByScoredFalseAndKickoffTimeBetween(Instant startTime, Instant endTime);
     
     // Check for exact duplicate
     Optional<Game> findByWeekAndHomeTeamAndAwayTeam(Integer week, String homeTeam, String awayTeam);
